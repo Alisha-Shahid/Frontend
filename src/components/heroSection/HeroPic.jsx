2a60deb87535ad6e7ee0ed360ab2481672a2cc73
Alisha Shahid <alishshahid7654@gmail.com>
2025-07-25 09:44:48 -0700

    fixed images

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/profile.png"
+        src="/images/profile.png"
         alt="Alisha Shahid"
         className="max-h-[450px] w-auto"
       />
